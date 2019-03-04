0db107b8fb885111e3a6bb3dcef3a2978b284a78
EC2 Default User <ec2-user@ip-172-31-35-50.us-east-2.compute.internal>
2019-03-04 13:51:09 +0000

    asdf

--- LC.tf
+++ LC.tf
@@ -1,4 +1,9 @@
 variable "environment" {}
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-2"
+}
 
 resource "aws_launch_configuration" "LC" {
     count = 3
